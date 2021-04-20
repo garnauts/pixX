8657e42957b84cb2c97705559605929d9cb9b26e
gtuxo <gcarlos.arnauts@dasa.com.br>
2021-04-19 23:00:33 -0300

    logo da extensão

--- upload/admin/language/en-gb/extension/payment/pixx.php
+++ upload/admin/language/en-gb/extension/payment/pixx.php
@@ -1,14 +1,16 @@
 <?php
 // Heading
-$_['heading_title']      = 'Pix X / Money Order';
+$_['heading_title']      = 'Pix X';
+
+$_['text_pixx']         = '<a href="http://www.arnauts.com.br" target="_blank"><img src="view/image/payment/pixx.png" alt="Pix X" title="Pix X"></a>';
 
 // Text
 $_['text_extension']     = 'Extensions';
-$_['text_success']       = 'Success: You have modified Pix X / money order account details!';
-$_['text_edit']          = 'Edit Pix X / Money Order';
+$_['text_success']       = 'Success: You have modified Pix X!';
+$_['text_edit']          = 'Edit Pix X';
 
 // Entry
-$_['entry_payable']      = 'Payable To';
+$_['entry_payable']      = 'Pix Key';
 $_['entry_total']        = 'Total';
 $_['entry_order_status'] = 'Order Status';
 $_['entry_geo_zone']     = 'Geo Zone';
@@ -19,5 +21,8 @@ $_['entry_sort_order']   = 'Sort Order';
 $_['help_total']         = 'The checkout total the order must reach before this payment method becomes active.';
 
 // Error
-$_['error_permission']   = 'Warning: You do not have permission to modify payment Pix X / money order!';
+$_['error_permission']   = 'Warning: You do not have permission to modify payment Pix X!';
 $_['error_payable']      = 'Payable To Required!';
+
+$_['entry_default_image'] = 'Upload QRCODE PIX';
+$_['entry_default_imageinfo'] = 'This image will be used for PIX QRCODE. If not loaded, the image will not appear.';
